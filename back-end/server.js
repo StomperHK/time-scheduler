273f0bac0ebae1373dd96811fa297bd252fbc1a1
Rafael Ribeiro <ra20fael04@gmail.com>
2025-03-06 19:05:19 -0300

    feat: create server.

---
+++ back-end/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { config } from "dotenv"
+import cors from "cors"
+
+const server = express()
+
+config()
+server.use(process.env.PRODUCTION === "true" ? cors({origin: /vercel\.app$/}) : cors({ origin: "*" }))
+
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running")
+})
