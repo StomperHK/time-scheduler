d3e8f9b9e0c914f07b72e678008c4d27e5eaad59
Rafael Ribeiro <ra20fael04@gmail.com>
2025-03-12 18:37:22 -0300

    fix: create landing page.

--- front-end/src/pages/main.js
+++ front-end/src/pages/main.js
@@ -1,1 +1,4 @@
+import logo from "../assets/logo.png"
 import "../style.css"
+
+document.querySelector('[data-js="logo"]').src = logo
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -18,5 +18,5 @@ export default defineConfig({
       }
     })
   ],
-  root: "src/pages",
+  root: "src/pages"
 })
