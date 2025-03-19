4e9faf009d1b352a87027718b6609544d1b69fd9
Rafael Ribeiro <ra20fael04@gmail.com>
2025-03-18 22:33:18 -0300

    refactor: prevent user from accessing premium page if he's not logged in.

--- front-end/src/pages/premium/main.js
+++ front-end/src/pages/premium/main.js
@@ -1,4 +1,15 @@
+import { validateUser } from "../../api/validateUser"
 import logo from "/assets/logo.png"
 import "/assets/style.css"
 
 document.querySelector('[data-js="logo"]').src = logo
+
+async function main() {
+  const userIsValid = await validateUser()
+
+  if (!userIsValid) {
+    location.href = "/login/"
+  }
+}
+
+main()
