4a0f1803992355bb6e7eb0d6b16eb4a7cf321732
Rafael Ribeiro <ra20fael04@gmail.com>
2025-03-25 19:48:01 -0300

    debug: add paymentData log.

--- back-end/lib/handleUserSubscription.js
+++ back-end/lib/handleUserSubscription.js
@@ -4,5 +4,7 @@ export async function handleUserSubscription(sql, paymentData) {
   const {user_id} = paymentData
   const userModel = new User(sql)
 
+  console.log(paymentData)
+
   userModel.upgradeUserToPremium(user_id)
 }
